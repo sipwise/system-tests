182a1bd65cd2a6068f157b4b224acfb6c403faa0
Alexander Lutay <alutay@sipwise.com>
2014-06-05 11:59:34 +0200

    MT#7333 Show list of cores if they exist

--- ce/03_no-cores.sh
+++ ce/03_no-cores.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 SYSCTL="/sbin/sysctl"
 CORE_DIR=$(dirname $(${SYSCTL} kernel.core_pattern | awk '{print $3}'| sed -e "s_'__g"))
+CORE_FILES=$(ls -1 -m ${CORE_DIR})
 NUM_FILES=$(ls -1 ${CORE_DIR}|wc -l)
 
 echo "1..1"
 
 if [ -d ${CORE_DIR} ]; then
 	if [[ $NUM_FILES > 0 ]]; then
-		echo "not ok 1 - ${CORE_DIR} not empty"
+		echo "not ok 1 - ${CORE_DIR} not empty: ${CORE_FILES}"
 	else
 		echo "ok 1 - ${CORE_DIR} empty"
 	fi
